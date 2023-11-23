6a9d2108f4d8db73d91ca0db4afc28274ef8461f
alemtzji <alejandro.martinezji@alumno.buap.mx>
2023-11-23 17:09:49 -0600

    carrito
    se agregan los productos al carrito

--- proyecto_final/lib/pantallas/carrito/CarritoCompras.dart
+++ proyecto_final/lib/pantallas/carrito/CarritoCompras.dart
@@ -1,27 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_final/pantallas/carrito/carrito.dart';
 
 class CarritoCompras extends StatelessWidget {
+  makeitem(BuildContext context, int index) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          contentPadding: EdgeInsets.all(20),
+          leading: Image.network(
+            cart.productos[index].imagenUrl,
+            height: double.infinity,
+            width: 100,
+            fit: BoxFit.contain,
+          ),
+          title: Text(cart.productos[index].nombre),
+          trailing: Text("\$${cart.productos[index].precio.toStringAsFixed(2)}",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16)),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: () {
+                    if (cart.productos[index].cantidadEnCarrito > 0) {
+                      // Reducir la cantidad en el carrito
+                      cart.productos[index].cantidadEnCarrito--;
+                      (context as Element).markNeedsBuild();
+                    }
+                  },
+                ),
+                Text(
+                  '${cart.productos[index].cantidadEnCarrito}', // Mostrar la cantidad en el carrito
+                  style: TextStyle(fontSize: 20),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    // Aumentar la cantidad en el carrito
+                    cart.productos[index].cantidadEnCarrito++;
+                    (context as Element).markNeedsBuild();
+                  },
+                ),
+          ],
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Carrito de Compras'),
+      body: ListView.separated(
+        itemBuilder: (ctx, index) => makeitem(context, index),
+        separatorBuilder: (ctx, index) => Divider(color: Colors.grey[300]),
+        itemCount: cart.obtenerCantidadTotal(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.shopping_cart,
-              size: 100,
-              color: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Tu carrito de compras está vacío',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
+      bottomSheet: Container(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            'Proceder al pago',
+          ),
         ),
       ),
     );
